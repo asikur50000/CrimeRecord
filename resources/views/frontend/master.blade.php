1cf74a8b132a050f4eb1a76ea541a780476234d0
asikur50000 <asikur50000@gmail.com>
2021-04-23 23:20:08 +0600

    saving to github

--- resources/views/frontend/master.blade.php
+++ resources/views/frontend/master.blade.php
@@ -5,12 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="{{asset('frontend')}}/style.css">
+    <link rel="stylesheet" href="{{asset('frontend')}}/css/design.css">
 </head>
 <body>
     <div class="banner">
         <div class="navbar">
-            <img src="{{asset('frontend')}}/logo.png" class="logo">
+            <img src="frontend/img/logo2.png" class="logo">
             <ul>
                 <li><a href="#">Home</a></li>
                 <li><a href="#">User</a></li>
@@ -18,9 +18,12 @@
                 <li><a href="http://localhost/CrimeRecord/public/admin">Admin</a></li>   
             </ul>
         </div>
+        <div class="projectname">
+            <h1>Crime Record Management System</h1>
+        </div>
 <div class="content">
     <h1>Thank You!</h1>
-    <p></p>
+    <p>Have a nice and safe day</p>
   <div>
       <button type="button"><span></span>About Us</button>
       <button type="button"><span></span>Feedback</button>
